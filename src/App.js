dbe10ed7beb2c7f4a12bd7ac8f10bf2c62a2354a
Lorenzo Furrer <>
2023-01-08 20:18:51 +0000

    added vocabulary set. styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { Col, Container, Row, Button } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
-import raw from './kanji_01.json';
+import kanjiRaw from './kanji_full.json';
+import vocabularyRaw from './vocabulary_full.json';
 import { QuestionAnswerComponent } from './components/QuestionAnswerComponent';
 
 function App() {
@@ -12,32 +13,46 @@ function App() {
   };
 
   const [appState, setAppState] = useState(AppState.SELECT_MODE);
-  const [fullDictionary] = useState([]);
+  const [fullKanjiDictionary] = useState([]);
+  const [fullVocabularyDictionary] = useState([]);
   const [kanjiSet, setKanjiSet] = useState([]);
 
   function loadKanjiDictionary() {
-    if (fullDictionary.length === 0) {
-      raw['data'].forEach(kanji => {
-        fullDictionary.push(kanji);
+    if (fullKanjiDictionary.length === 0) {
+      kanjiRaw['data'].forEach(kanji => {
+        fullKanjiDictionary.push(kanji);
       });
-      console.log('Loaded', fullDictionary.length, 'kanjis in the dictionary');
+      console.log('Loaded', fullKanjiDictionary.length, 'kanjis in the dictionary');
+    }
+  }
+
+  function loadVocabularyDictionary() {
+    if (fullVocabularyDictionary.length === 0) {
+      vocabularyRaw['data'].forEach(vocab => {
+        fullVocabularyDictionary.push(vocab);
+      });
+      console.log('Loaded', fullVocabularyDictionary.length, 'vocabs in the dictionary');
     }
   }
 
   useEffect(() => {
     loadKanjiDictionary();
+    loadVocabularyDictionary();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  function handleKanjiSetSelection(event) {
+  function handleSetSelection(event) {
     const dataOption = event.target.getAttribute('data-option');
     let selectedSet = [];
     switch (dataOption) {
+      case "full-vocab":
+        selectedSet = fullVocabularyDictionary;
+        break;
       case "full":
-        selectedSet = fullDictionary;
+        selectedSet = fullKanjiDictionary;
         break;
       default:
-        selectedSet = fullDictionary
+        selectedSet = fullKanjiDictionary
           .filter(kanji => kanji['data'].hasOwnProperty('categories'))
           .filter(kanji => kanji['data']['categories'].includes(dataOption));
         break;
@@ -51,47 +66,34 @@ function App() {
     setAppState(AppState.SELECT_MODE);
   }
 
+  function SelectionOption(props) {
+    return (
+      <Row>
+        <Col>
+          <Button onClick={props.handleSetSelectionCallback} data-option={props.dataOption}>
+            {props.children}
+          </Button>
+        </Col>
+      </Row>
+    );
+  }
+
   function SelectMode() {
     return (
       <Row>
         <Col className='App-body'>
           <Row>
-            <Col>
+            <Col className='select-title'>
               Select Kanji set:
             </Col>
           </Row>
-          <Row>
-            <Col>
-              <Button onClick={handleKanjiSetSelection} data-option='jlpt5'>
-                JLPT N5
-              </Button>
-            </Col>
-            <Col>
-              <Button onClick={handleKanjiSetSelection} data-option='jlpt4'>
-                JLPT N4
-              </Button>
-            </Col>
-            <Col>
-              <Button onClick={handleKanjiSetSelection} data-option='jlpt3'>
-                JLPT N3
-              </Button>
-            </Col>
-            <Col>
-              <Button onClick={handleKanjiSetSelection} data-option='jlpt2'>
-                JLPT N2
-              </Button>
-            </Col>
-            <Col>
-              <Button onClick={handleKanjiSetSelection} data-option='full'>
-                Full Set
-              </Button>
-            </Col>
-            <Col>
-              <Button onClick={handleKanjiSetSelection} data-option='test'>
-                Test
-              </Button>
-            </Col>
-          </Row>
+          <SelectionOption handleSetSelectionCallback={handleSetSelection} dataOption={'jlpt5'}>JLPT N5</SelectionOption>
+          <SelectionOption handleSetSelectionCallback={handleSetSelection} dataOption={'jlpt4'}>JLPT N4</SelectionOption>
+          <SelectionOption handleSetSelectionCallback={handleSetSelection} dataOption={'jlpt3'}>JLPT N3</SelectionOption>
+          <SelectionOption handleSetSelectionCallback={handleSetSelection} dataOption={'jlpt2'}>JLPT N2</SelectionOption>
+          <SelectionOption handleSetSelectionCallback={handleSetSelection} dataOption={'full'}>Full Kanji Set</SelectionOption>
+          <SelectionOption handleSetSelectionCallback={handleSetSelection} dataOption={'full-vocab'}>Full Vocabulary Set</SelectionOption>
+          <SelectionOption handleSetSelectionCallback={handleSetSelection} dataOption={'test'}>Test</SelectionOption>
         </Col>
       </Row>
     );
